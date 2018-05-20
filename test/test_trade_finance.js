302433f5b56f1d4b40d6be822586651968e57500
whunmr <whunmr@gmail.com>
2018-05-20 23:46:22 +0800

    refactor tests

--- test/test_trade_finance.js
+++ test/test_trade_finance.js
@@ -5,106 +5,49 @@ const MetaCoin = artifacts.require("MetaCoin");
 const TradeFinance = artifacts.require("TradeFinance");
 
 function assert_struct_eq(a1, a2) {
-    return assert.equal(JSON.stringify(a1), JSON.stringify(a2));
+  return assert.equal(JSON.stringify(a1), JSON.stringify(a2));
 }
 
-contract('Testing for TradeFinance Contract', async (accounts) => {
-  var instance;
-    
-  beforeEach(async () => {
-    instance = await TradeFinance.deployed();
-  });
-  
-  afterEach(async () => {
-  });
+contract('Testing for TradeFinance Contract', async(accounts) => {
+  let seller = accounts[0];
+  let buyer = accounts[1];
+  let price = 1e19;
+  let digest = "86d3f3a95c324c9479bd8986968f4327";
+
     
   it("should contains zero order in contract after deployed", async() => {
-    let order_count = await instance.order_count();
+    let ___c = await TradeFinance.deployed();
+    let order_count = await ___c.order_count();
     assert.equal(order_count, 0);
   })
 
-  it("should able to create order", async() => {
-    let seller = accounts[0];
-    let buyer = accounts[1];
-
-    let price = 10000000000000000000;
-    let digest = "86d3f3a95c324c9479bd8986968f4327";
-
-    let order_id = await instance.create_order.call(buyer, price, digest);
-    let tx       = await instance.create_order(buyer, price, digest);
-    let order = await instance.orders.call(order_id);
-
-    assert_struct_eq( order
-                    , [order_id, accounts[0], accounts[1], price.toString(), "0", digest, false]);
-
+    
+  it("should able to create order, deposit and release escrow.", async() => {
+    let ___c = await TradeFinance.deployed();
+    let order_id = await ___c.create_order.call(buyer, price, digest);
+    let tx       = await ___c.create_order     (buyer, price, digest);
+      
     truffleAssert.eventEmitted(tx, 'OrderCreated', (ev) => {
       return ev.ricardian_digest == digest;
     });
 
+    //////////////////////////////////////////////////////////////////////////////
+    let deposit_amount = 2e19;
+    await ___c.deposit(order_id, {value: deposit_amount, from: buyer});
 
-    let escrow_value = 20000000000000000000;
-    await instance.deposit(order_id, {value: escrow_value, from: buyer});
-      
-      
-    order = await instance.orders.call(order_id);
+    let order = await ___c.orders.call(order_id);
     assert_struct_eq( order
-                      , [order_id, accounts[0], accounts[1], price.toString()
-                      , escrow_value.toString(), digest, false]);
+                    , [order_id, seller, buyer, price.toString()
+                    , deposit_amount.toString(), digest, false]);
+      
+    //////////////////////////////////////////////////////////////////////////////
+    tx = await ___c.release_escrow(order_id, {from: buyer});
 
-    let expected_balance = escrow_value;
-    let actual_balance = web3.eth.getBalance(instance.address);
-    assert.equal(actual_balance, expected_balance);
+    assert.equal((await ___c.orders.call(order_id))[6]/*.paid*/, true);
 
-    result = await instance.release_escrow(order_id, {from: buyer});
-
-    truffleAssert.eventEmitted(result, 'EscrowReleased', (ev) => {
+    truffleAssert.eventEmitted(tx, 'EscrowReleased', (ev) => {
       return ev.order_id.toNumber() == order_id;
     });
-
   })
-    
-  //it("should put 10000 MetaCoin in the first account", async () => {
-  //   let instance = await MetaCoin.deployed();
-  //   let balance = await instance.getBalance.call(accounts[0]);
-  //   assert.equal(balance.valueOf(), 10000);
-  //})
-  //
-  //it("should call a function that depends on a linked library", async () => {
-  //  let meta = await MetaCoin.deployed();
-  //  let outCoinBalance = await meta.getBalance.call(accounts[0]);
-  //  let metaCoinBalance = outCoinBalance.toNumber();
-  //  let outCoinBalanceEth = await meta.getBalanceInEth.call(accounts[0]);
-  //  let metaCoinEthBalance = outCoinBalanceEth.toNumber();
-  //  assert.equal(metaCoinEthBalance, 2 * metaCoinBalance);
-  //});
-  //
-  //it("should send coin correctly", async () => {
-  //
-  //  // Get initial balances of first and second account.
-  //  let account_one = accounts[0];
-  //  let account_two = accounts[1];
-  //
-  //  let amount = 10;
-  //
-  //
-  //  let instance = await MetaCoin.deployed();
-  //  let meta = instance;
-  //
-  //  let balance = await meta.getBalance.call(account_one);
-  //  let account_one_starting_balance = balance.toNumber();
-  //
-  //  balance = await meta.getBalance.call(account_two);
-  //  let account_two_starting_balance = balance.toNumber();
-  //  await meta.sendCoin(account_two, amount, {from: account_one});
-  //
-  //  balance = await meta.getBalance.call(account_one);
-  //  let account_one_ending_balance = balance.toNumber();
-  //
-  //  balance = await meta.getBalance.call(account_two);
-  //  let account_two_ending_balance = balance.toNumber();
-  //
-  //  assert.equal(account_one_ending_balance, account_one_starting_balance - amount, "Amo");
-  //  assert.equal(account_two_ending_balance, account_two_starting_balance + amount, "Amo");
-  //});
-
+  
 })
